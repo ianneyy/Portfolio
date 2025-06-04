23678c550360d1bd5fba3aebc9c1be76303f941f
ianneyy <jimmygomez1230@gmail.com>
2025-06-04 16:08:54 +0800

    fix responsiveness

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -33,16 +33,17 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10 mb-48"
+      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10 mb-10 lg:mb-48"
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Changelog from my journey
+          Highlights from My Tech Journey
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+          As a college student passionate about technology, I’ve been building
+          projects, exploring machine learning, and solving real-world problems.
+          Here’s a timeline of the key milestones in my journey so far.
         </p>
       </div>
 
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -9,7 +9,7 @@ import { Hero } from "@/components/hero";
 
 const Skills = () => {
   return (
-    <div className=" mt-52">
+    <div className="mt-52">
       <h1 className="flex justify-center text-4xl">SKILLS</h1>
       <p className="mt-2 text-center text-muted-foreground text-lg mb-10">
         A showcase of my technical abilities and tools I work with.
